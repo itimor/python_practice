a89693d2ac6d50b1f8d20161db7134d7d76febc7
卢建文 <763836801@qq.com>
2017-03-31 13:32:37 +0800

    加记录操作日志,不过未完善

--- cmd_console/demo/views.py
+++ cmd_console/demo/views.py
@@ -7,6 +7,44 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 import json
 import sys
+from .models import Record
+
+
+def _record(record_list):
+    '''save action record to db'''
+
+    record = Record(user=record_list['user'],
+                    brand=record_list['brand'],
+                    from_ip=record_list['from_ip'],
+                    cmd=record_list['cmd'],
+                    action=record_list['action'])
+    record.save()
+
+
+def record(request):
+    '''view action record to db'''
+
+    record_fileds = Record._meta.get_fields()
+    banned_fields = ['ID', u'命令', u'动作']
+    filter_fields = {f.verbose_name: f.name for f in record_fileds
+                     if f.verbose_name not in banned_fields}
+    filter_str = str(datetime.now()).split()[0]
+    filter_field = 'action_time'
+    if request.POST.get('filter_str') and request.POST.get('filter_field'):
+        filter_str = request.POST.get('filter_str')
+        filter_field = filter_fields[request.POST.get('filter_field')]
+    filter_field_contains = '%s__contains' % filter_field
+    my_filter = {}
+    my_filter[filter_field_contains] = filter_str
+    records = Record.objects.filter(**my_filter) \
+        .order_by('-action_time', 'user').values()
+
+    context = {
+        'records': records,
+        'filter_fields': filter_fields
+    }
+    return render(request, "record.html", context)
+
 
 def _action(cmd):
     '''
@@ -23,30 +61,31 @@ def _action(cmd):
     result['stderr'] = stderr
     return result
 
+
 def action(request):
     if request.method == 'POST' and request.is_ajax():
         result = request.POST
-        
+
         # cmd = ['ping', '-c', '1', 'www.baidu.com']  # 测试命令，你可以定义的更复杂
         cmd = result['btncmd'].split()
-        print cmd
         cmd_exec = _action(cmd)  # 调用执行函数，获得结果
         stdout = cmd_exec['stdout']
         stderr = cmd_exec['stderr']
         if not stdout:
             stdout = stderr
-        # else:
-        #     try:
-        #         record_list = {
-        #             'user': request.POST['user'],
-        #             'brand': selbrand,
-        #             'from_ip': user_ip(request),
-        #             'cmd': ' '.join(cmd),
-        #             'action': 'TR',
-        #         }
-        #     except:
-        #         print sys.exc_info()[0]
-        #     _record(record_list)
+            # 保存操作记录
+            # else:
+            #    try:
+            #        record_list = {
+            #            'user': result['user'],
+            #            'brand': result['selbrand',
+            #            'from_ip': user_ip(request),
+            #            'cmd': ' '.join(cmd),
+            #            'action': 'TR',
+            #        }
+            #    except:
+            #        print sys.exc_info()[0]
+            #    _record(record_list)
 
         try:
             stdout = str(stdout)
@@ -58,6 +97,7 @@ def action(request):
         return HttpResponse(json.dumps(stdout),
                             content_type="application/json")
     return HttpResponseNotFound('<h1>Page not found</h1>')
+
 
 def demo(request):
     return render(request, "demo.html")
